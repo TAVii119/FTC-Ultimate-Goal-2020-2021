096577c2d224e0798b9c8d7b5ea35418daa01dfc
TAVii119 <tavigames@gmai.com>
2020-10-28 08:52:14 +0200

    Modified the directory of the RampMotorSpeed class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RampMotorSpeed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RampMotorSpeed.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.roadrunner.drive;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
